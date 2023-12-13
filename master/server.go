cc2db051816da3a12f24bb431d5637eb4101b2b1
Victor1319 <834863182@qq.com>
2023-12-13 09:52:40 +0800

    oppo self commit
     #22179575
    Signed-off-by: Victor1319 <834863182@qq.com>

--- master/server.go
+++ master/server.go
@@ -73,7 +73,7 @@ var (
 	ownerRegexp   = regexp.MustCompile("^[A-Za-z][A-Za-z0-9_]{0,20}$")
 
 	useConnPool           = true // for test
-	enableDirectDeleteVol = true
+	enableDirectDeleteVol = false
 	gConfig               *clusterConfig
 )
 
@@ -395,7 +395,7 @@ func (m *Server) checkConfig(cfg *config.Config) (err error) {
 	}
 	m.config.volDeletionDentryThreshold = uint64(threshold)
 
-	enableDirectDeleteVol = cfg.GetBoolWithDefault(cfgEnableDirectDeleteVol, true)
+	enableDirectDeleteVol = cfg.GetBoolWithDefault(cfgEnableDirectDeleteVol, false)
 
 	memPercent := cfg.GetString(cfgMetaNodeMemoryHighPer)
 	if memPercent != "" {
