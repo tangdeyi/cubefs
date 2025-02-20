20f0bb0879a42ed341292247df6e8a352e596b9a
baihailong <baihailong@oppo.com>
2025-02-20 17:50:41 +0800

    fix(sdk): add some log.#23042874
    Signed-off-by: baihailong <baihailong@oppo.com>

--- client/libsdk/libsdk.go
+++ client/libsdk/libsdk.go
@@ -1102,6 +1102,7 @@ func cfs_refreshsummary(id C.int64_t, path *C.char, goroutine_num C.int, unit *C
 	goroutineNum := int32(goroutine_num)
 	err = c.mw.RefreshSummary_ll(ino, goroutineNum, C.GoString(unit), C.GoString(split))
 	if err != nil {
+		log.LogErrorf("cfs_refreshsummary failed, path(%v) err(%v)", c.absPath(C.GoString(path)), err)
 		return errorToStatus(err)
 	}
 	return statusOK
@@ -1463,6 +1464,7 @@ func cfs_getsummary(id C.int64_t, path *C.char, summary *C.struct_cfs_summary_in
 
 	info, err := c.lookupPath(c.absPath(C.GoString(path)))
 	if err != nil {
+		log.LogErrorf("cfs_getsummary not found path(%v) err(%v)", c.absPath(C.GoString(path)), err)
 		return errorToStatus(err)
 	}
 
@@ -1486,6 +1488,7 @@ func cfs_getsummary(id C.int64_t, path *C.char, summary *C.struct_cfs_summary_in
 	goroutineNum := int32(goroutine_num)
 	summaryInfo, err := c.mw.GetSummary_ll(info.Inode, goroutineNum)
 	if err != nil {
+		log.LogErrorf("cfs_getsummary failed, path(%v) err(%v)", c.absPath(C.GoString(path)), err)
 		return errorToStatus(err)
 	}
 	if strings.ToLower(C.GoString(useCache)) != "false" {
